b560cbaecfc28ffcec2e526e2e194f10ba857a76
Chip Schweiss <chip@databento.com>
2025-08-01 06:06:13 -0500

    Enable USB Recovery

--- mx5300/initial_setup.sh
+++ mx5300/initial_setup.sh
@@ -125,4 +125,12 @@ wait_for_reboot
 
 setup_openwrt
 
+# Enable USB recovery
+echo
+echo "Enabling USB recovery."
+
+fw_setenv usbimage 'openwrt-qualcommax-ipq807x-linksys_mx5300-initramfs-uImage.itb'
+fw_setenv bootusb 'usb start && fatload usb 0:1 $loadaddr $usbimage && bootm $loadaddr'
+fw_setenv bootcmd 'run bootusb; aq_load_fw && if test $auto_recovery = no; then bootipq; elif test $boot_part = 1; then run bootpart1; else run bootpart2; fi'
+
 echo "Openwrt basic setup complete on both partitons."
